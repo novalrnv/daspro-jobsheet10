c4a1b8d43862572b3294cc8e3e9f92fb6cd0fe1e
novalrnv <akuunoval@gmail.com>
2024-11-07 12:05:14 +0700

    modify 3

--- Numbers12.java
+++ Numbers12.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Numbers12 {
     public static void main(String[] args) {
         
@@ -5,5 +7,13 @@ public class Numbers12 {
         mynumbers[0] = new int[5];
         mynumbers[1] = new int[3];
         mynumbers[2] = new int[1];
+
+        for (int i = 0; i < mynumbers.length; i++) {
+            System.out.println(Arrays.toString(mynumbers[i]));
+        }
+
+        for (int i = 0; i < mynumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + " : " + mynumbers[i].length);
+        }
     }
 }
